bb02da549072bc5be378a6fc90288532c8b5dfff
CrimsonCompiler <toisiftasrik@gmail.com>
2026-09-21 09:36:47 +0600

    AppUser Entity Model created

---
+++ FlashSale.Api/Models/AppUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FlashSale.Api.Models;
+
+public class AppUser: IdentityUser
+{
+    public string FullName  { get; set; } = string.Empty;
+    public string Role { get; set; } = "Customer";
+}
